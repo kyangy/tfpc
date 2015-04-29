cbd0886f8c4a6098940c0e925aa9804eb3a1c238
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-29 15:11:54 -0700

    added routes for user and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   
+  resources :users, only: [:create]
+  get '/signup' => 'users#new', as: :signup
+
+  get '/login'     => 'sessions#new', as: :login
+  post '/login'    => 'sessions#create', as: :create_login
+  delete '/logout' => 'sessions#destroy', as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
