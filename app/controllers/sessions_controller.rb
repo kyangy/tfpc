9ea086582a08ed078137bf6a4e4317b5efce1136
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-29 15:11:11 -0700

    created sessions methods and included sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	  user = User.where(email: params[:login][:email]).first
+	  if user && user.authenticate(params[:login][:password])
+	    log_in(user)
+	    redirect_to root_path
+	  else
+	    flash[:notice] = "Please Try Again"
+	    redirect_to login_path
+	  end
+	end
+
+	def destroy
+	  log_out if logged_in?
+	  redirect_to root_path
+	end
 end
