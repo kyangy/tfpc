a6aa1b505d090abe8bdac6d84fb6ebb71294ca8c
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-05-08 12:02:02 -0700

    created events controller

--- db/migrate/20150508185420_create_events.rb
+++ db/migrate/20150508185420_create_events.rb
@@ -1,6 +1,11 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :title
+      t.string :description
+      t.string :image
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429220652) do
+ActiveRecord::Schema.define(version: 20150508185420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "image"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first"
