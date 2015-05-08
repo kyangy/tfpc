cccc1e85d231f02f58967fd2e2b32d3334c1ac09
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-05-08 12:06:55 -0700

    included events controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get '/about' => 'about_pages#home'
   get '/connect' => 'connect_pages#home'
+
+  resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
