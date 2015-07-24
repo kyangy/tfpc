cd6590aeb1ad0b0a5edfcff7621ae832b05fad56
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-07-24 08:00:06 -0700

    working announcement upload

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,31 @@
 class AnnouncementsController < ApplicationController
+
+	def index
+		@announcements = Announcement.all
+	end
+
+	def new
+		@announcement = Announcement.new	
+	end
+
+	def create
+		@announcement = Announcement.new(announcement_params)
+		if @announcement.save
+			redirect_to announcements_path
+		else
+			redirect_to :new
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def announcement_params
+		params.require(:announcement).permit(:image)
+	end
 end
