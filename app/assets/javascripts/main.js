b5c920783805c77b19acbc7509bfe6e789ae1ed3
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-07-29 12:05:00 -0700

    changing styles

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".main-info").hover(function(){
+		$(this).addClass("animated rubberBand");
+	}, function(){
+		$(this).removeClass("animated rubberBand");
+	})
+
+});
